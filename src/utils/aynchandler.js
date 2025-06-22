1654aaac0928e0cc1d035083a869f2db7f914a9f
Faizan Ali <alammdfaizan50@gmail.com>
2025-06-22 13:53:31 +0530

    handle api response with promise

--- src/utils/aynchandler.js
+++ src/utils/aynchandler.js
@@ -10,13 +10,15 @@ export { asyncHandler }
 // const asyncHandler = (func) => {() => {}}
 // const asyncHandler = (func) => async () => {}
 
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     }catch(error){
-//         res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message
-//         })
-//     }
-// }
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    }catch(error){
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+*/
