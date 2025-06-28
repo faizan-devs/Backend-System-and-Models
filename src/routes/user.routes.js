b72824ee26ddd05ad69aa9ba828b776328d875f8
Faizan Ali <alammdfaizan50@gmail.com>
2025-06-28 18:03:31 +0530

    fix middleware error

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middelware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
