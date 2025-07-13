cda828b8161d624fe2d8600bd18293ebb2dfc8a4
Faizan Ali <alammdfaizan50@gmail.com>
2025-07-13 23:58:23 +0530

    fix: correct import casing for ApiError and update TODO comments for consistency

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,7 +1,7 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import { Video } from "../models/video.model.js"
 import { User } from "../models/user.model.js"
-import { ApiError } from "../utils/ApiError.js"
+import { ApiError } from "../utils/apiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
@@ -24,8 +24,7 @@ const getVideoById = asyncHandler(async (req, res) => {
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
-
+    //TODO: update video details like title, description, thumbnail 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
@@ -35,6 +34,7 @@ const deleteVideo = asyncHandler(async (req, res) => {
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    // TODO: toggle publish status
 })
 
 export {
